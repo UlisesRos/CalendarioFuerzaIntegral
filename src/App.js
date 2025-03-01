efe6092ac4a58b5b5284853c3f71fe5e028e3494
Ulises Ros <ulisesros70@gmail.com>
2025-03-01 14:02:19 -0300

    errores

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
 
     axios.interceptors.request.use((config) => {
         const token = localStorage.getItem('token');
+        console.log('token', token)
         if (token) {
             config.headers['Authorization'] = `Bearer ${token}`;
         }
